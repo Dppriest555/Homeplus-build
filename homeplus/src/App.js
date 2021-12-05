faa7e0b0b64ddb0a5ba0cbb95766c308dac3fd56
JRoshthen1 <jroshthen1@gmail.com>
2021-12-05 19:45:06 +0100

    app init and calendar

---
+++ homeplus/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calendar from 'react-calendar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
