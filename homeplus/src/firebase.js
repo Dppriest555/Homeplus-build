70d136739a4a6498026f516f0df5114b011fd679
JRoshthen1 <jroshthen1@gmail.com>
2021-12-07 11:25:55 +0100

    firebase env

--- homeplus/src/firebase.js
+++ homeplus/src/firebase.js
@@ -1,8 +1,8 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import { firebase } from "firebase/app"
+import {getAuth} from "firebase/auth"
 
 
-const app = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -10,10 +10,9 @@ const app = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-})
+}
 
-const analytics = getAnalytics(app);
+const app = firebase(firebaseConfig)
 
-export const auth = app.auth()
-export default app
-export default analytics
+export const auth = getAuth(app)
+
