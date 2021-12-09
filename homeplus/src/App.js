3346f264f9c20c081a64a43e1593ec3095534049
Dppriest555 <ivan.velkov01@abv.bg>
2021-12-09 16:22:17 +0100

    Routing

--- homeplus/src/App.js
+++ homeplus/src/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import './styles/App.css';
 import './styles/Calendar.css';
 import Signup from './components/Signup';
+import CalendarComponent from './components/Calendar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
+    <Router >
     <div className="App">
         <div className="container">
-          <Signup />
+          <Switch>
+            <Route path="/">
+              <Signup />
+              < CalendarComponent />
+            </Route>
+            
+          </Switch>
         </div>
     </div>
+    </Router >
   );
 }
 
