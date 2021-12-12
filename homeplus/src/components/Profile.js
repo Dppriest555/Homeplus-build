d4c081aa03a5883419f88f3f9a37b2ddd545486c
JRoshthen1 <jroshthen1@gmail.com>
2021-12-12 14:41:31 +0100

    Update Profile.js

--- homeplus/src/components/Profile.js
+++ homeplus/src/components/Profile.js
@@ -5,17 +5,52 @@ import { useState } from 'react'
 
 
 
-
 function Profile() {
 
 
-    const auth = getAuth();
-    const [user, setUser] = useState({});
 
 
-    onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser);
-      });
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+
+    
+
+
+
+
+
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+const [displayName, setName] = useState("");
+
+
+
+const update = async () => {
+    try {
+      const user = await updateProfile(
+        auth,
+        displayName,
+      );
+      console.log(user.displayName);
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  };
+      
+
+
+
+
 
 
 
@@ -23,6 +58,17 @@ function Profile() {
     return (
         <div>            
             <h2>
+            <input
+                placeholder="Name..."
+                type="text"
+                onChange={(event) => {
+                setName(event.target.value);
+                  }}
+                />
+
+    <button className="btn" onClick={update}>Log in</button>
+
+
                 {user.uid}
                 <br/>
                 {user.email}
